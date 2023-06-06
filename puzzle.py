4371510ce236088e5566ff7b53adec76444c3389
Siebert Lubbe <siebertlubbe@Sieberts-MacBook-Pro.local>
2023-06-06 12:21:29 +1000

    Adding the implication of a true statement

--- puzzle.py
+++ puzzle.py
@@ -19,6 +19,7 @@ knowledge0 = And(
     Not(And(AKnave, BKnave)),
     Not(And(AKnight, AKnave)),
     Not(And(BKnight, BKnave)),
+    Implication(And(AKnight, AKnave), And(AKnight, AKnave)),
     Implication(Not(And(AKnight, AKnave)), (AKnave))
 )
 
